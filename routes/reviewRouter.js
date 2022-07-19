6f3d0e6c2db690a66498fe420cec445bfae2f390
Pulatoff <niyozbekpulatov@gmail.com>
2022-07-19 17:23:39 +0500

    bug fix

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const controller = require("../controller/reviewController");
+
 router.route("/").get(controller.getAllReviews).post(controller.addReview);
+router
+  .route("/:id")
+  .get(controller.getOneReview)
+  .patch(controller.updateReview)
+  .delete(controller.deleteReview);
 
 module.exports = router;
